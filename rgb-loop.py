22aad1b3c6cae62f0036e6ac09909da4ae4cf57a
lukaszkomar <l_komr@wp.pl>
2014-12-15 23:47:28 +0100

    ignore + ide test

--- rgb-loop.py
+++ rgb-loop.py
@@ -1,24 +1,26 @@
 import RPi.GPIO as GPIO
+from datetime import time
 
-GREEN = 18; 
-BLUE  = 17; 
+GREEN = 18
+BLUE  = 17
 
-#GPIO.setmode(GPIO.BOARD);
-GPIO.setmode(GPIO.BCM);
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(BLUE , GPIO.OUT);
-GPIO.setup(GREEN, GPIO.OUT);
+GPIO.setup(BLUE , GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
 
 #GPIO.output(BLUE, 1);
 #GPIO.output(BLUE, 1);
 
 try:
-    while(True):
-        GPIO.output(GREEN, 1);
-        
-        GPIO.output(GREEN, 0);
-        GPIO.output(BLUE , 1);
-        GPIO.output(BLUE , 0);
+    while True:
+        GPIO.output(GREEN, 1)
+        time.sleep(2)
+        GPIO.output(GREEN, 0)
+        GPIO.output(BLUE , 1)
+        time.sleep(2)
+        GPIO.output(BLUE , 0)
 
 except KeyboardInterrupt:
-    GPIO.cleanup();
+    GPIO.cleanup()
--- rgb.py
+++ rgb.py
@@ -1,23 +1,23 @@
 import RPi.GPIO as GPIO
 
-GREEN = 18; 
-BLUE  = 17; 
+GREEN = 18
+BLUE  = 17
 
-#GPIO.setmode(GPIO.BOARD);
-GPIO.setmode(GPIO.BCM);
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(BLUE , GPIO.OUT);
-GPIO.setup(GREEN, GPIO.OUT);
+GPIO.setup(BLUE , GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
 
 #GPIO.output(BLUE, 1);
 #GPIO.output(BLUE, 1);
 
 try:
     while(True):
-        request = raw_input("_GB ");
+        request = raw_input("_GB ")
         if (len(request) == 2):
-            GPIO.output(GREEN, int(request[0]));
-            GPIO.output(BLUE , int(request[1]));
+            GPIO.output(GREEN, int(request[0]))
+            GPIO.output(BLUE , int(request[1]))
 
 except KeyboardInterrupt:
-    GPIO.cleanup();
+    GPIO.cleanup()
